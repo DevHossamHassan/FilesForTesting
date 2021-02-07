c4fc70b08dbd8016ee057dc694af9fd35c58a76e
DevHossamHassan <hossamhassancs15@gmail.com>
2021-02-07 23:26:38 +0100

    Introduce VCardsActivity

--- app/src/main/java/com/letsgotoperfection/filesfortesting/MainActivity.kt
+++ app/src/main/java/com/letsgotoperfection/filesfortesting/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.letsgotoperfection.filesfortesting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnVCards: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_FilesForTesting_Light)
         setContentView(R.layout.activity_main)
+        btnVCards = findViewById(R.id.btnVCards)
+        btnVCards.setOnClickListener {
+            startActivity(Intent(this, VCardsActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/letsgotoperfection/filesfortesting/VCardsActivity.kt
@@ -0,0 +1,11 @@
+package com.letsgotoperfection.filesfortesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VCardsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vcards)
+    }
+}
